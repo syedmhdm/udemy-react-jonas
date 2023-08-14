1a007f73023ffdfabe56c92f8159c0d67949672d
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-08-14 23:20:28 +0530

    We change the state, we change the UI, state is cool

---
+++ 04-steps/starter/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
+  return (
+    <div className='steps'>
+      <div className='numbers'>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <p className='message'>
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className='buttons'>
+        <button
+          onClick={handlePrevious}
+          style={{ background: "#7950f2", color: "#fff" }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          style={{ background: "#7950f2", color: "#fff" }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
